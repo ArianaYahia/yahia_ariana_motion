4cd4d762c392cd9ae20d38e5c2b90cbfc6ad343f
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2021-01-27 10:37:58 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ while True:
     print(input.rotation(Rotation.PITCH))
     if input.rotation(Rotation.PITCH) > 9 or input.rotation(Rotation.PITCH) < -9:
        light.show_animation(light.rainbow_animation, 100)
+    else:
+        light.clear()
--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ while (true) {
     console.log(input.rotation(Rotation.Pitch))
     if (input.rotation(Rotation.Pitch) > 9 || input.rotation(Rotation.Pitch) < -9) {
         light.showAnimation(light.rainbowAnimation, 100)
+    } else {
+        light.clear()
     }
     
 }
